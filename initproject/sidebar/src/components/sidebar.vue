27533773e0b44403c07a1b60642a87546bc4ff67
pengxuemei <xuemei.peng@sipingsoft.com>
2020-02-25 08:49:21 +0800

    样式调整

--- initproject/sidebar/src/components/sidebar.vue
+++ initproject/sidebar/src/components/sidebar.vue
@@ -1,13 +1,12 @@
 <template>
   <div class='container'>
-    <div style='display: inline-block; vertical-align: middle; center; width: 30%'>
+    <div style='display: inline-block; vertical-align: top; center; width: 30%'>
       <el-menu default-active="1"
         :router='true'
         :unique-opened="true"
         background-color="#545c64"
         text-color="#fff"
-        active-text-color="#ffd04b"
-        >
+        active-text-color="#ffd04b">
         <template v-for='item in routers'>
           <el-menu-item  @click='selectMenu(item)' :index="item.path" v-if='item.children.length === 0' :key='item.meta.title'>{{ item.meta.title }}</el-menu-item>
           <el-submenu :index="item.path" v-else-if='item.children.length > 0' :key='item.meta.title'>
@@ -19,7 +18,7 @@
         </template>
     </el-menu>
     </div>
-    <div style='display: inline-block; vertical-align: middle; width: 68%'>
+    <div style='display: inline-block; vertical-align: top; width: 68%'>
     <el-tabs v-model="active" type="card" closable @tab-remove="removeTab">
       <el-tab-pane
       :key="item.meta.title"
--- initproject/sidebar/src/main.js
+++ initproject/sidebar/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router/index'
 import VueRouter from 'vue-router'
 import elementui from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(elementui)
